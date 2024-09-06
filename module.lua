1769a466b0596f9dd0e5be2d395da5920974029b
OnlineOnAllTime <107973259+OnlineOnAllTime@users.noreply.github.com>
2024-09-06 05:05:36 +0200

    Update module.lua

--- module.lua
+++ module.lua
@@ -44,21 +44,6 @@ module.CreateEmbed = function(Player, title, description, arg1, arg2, url,image)
 	end
 end
 
---[[
-	
-	webhookService.CreateEmbed(plr, title, description, {
-		['name'] = "name", -- Embed 1
-		['value'] = "value",
-		['inline'] = true,
-	}, {
-		['name'] = "Reason:",
-		['value'] =  Reason,
-		['inline'] = true,
-	}, url,"url of image") 
-	webhookService.CreateMessage("@everyone",url)
 
-
-
-]]
 
 return module
